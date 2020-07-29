098d4043f2425bb864224661d52d1e22a29e6a88
saraswathimariappan <66959124+saraswathimariappan@users.noreply.github.com>
2020-07-29 19:35:42 +0530

    Day2 Python Assignment

---
+++ Day2PYassignment.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello world");
+
+
+# In[2]:
+
+
+print("hellopython");
+
+
+# In[3]:
+
+
+print("""()()()""");
+
+
+# slash and triple code completed
+# \t\n--for space
+# type of printing values
+# variable assignment
+# operators(arithmetic,bitwise,comparison)
+# id--ram
+# 
+
+# In[13]:
+
+
+name='dia';
+print("name is \n",name);
+
+print(f"name is {name}");
+
+
+# In[18]:
+
+
+x=4
+y=40
+
+
+# In[15]:
+
+
+print(x);
+
+
+# In[19]:
+
+
+x=x+y;
+print(x);
+
+
+# In[20]:
+
+
+id(x);
+
+
+# In[21]:
+
+
+print(id(x));
+
+
+# In[ ]:
+
+
+
+
